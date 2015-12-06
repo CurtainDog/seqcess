f49c167a27b342872d7e4970bd7a6f2cf19e3612
CurtainDog <richardc_2074@yahoo.com>
2015-12-06 18:29:54 +1100

    Added Objective Arrow

--- src/seqcess/core.clj
+++ src/seqcess/core.clj
@@ -2,28 +2,42 @@
 
 (defmacro seq->
   "Seqing Arrow. Takes a destructuring form and repeatedly binds
-  the form to a sequence of the cumulative results made by
-  evaluating each form in turn. Returns the entire sequence.
-  The behaviour is similar to as-> except that it always binds
-  to a sequence and returns a sequence."
+			  the form to a sequence of the cumulative results made by
+			  evaluating each form in turn. Returns the entire sequence.
+			  The behaviour is similar to as-> except that it always binds
+			  to a sequence and returns a sequence."
   ([s & forms]
-   (let [temp (gensym)
-         temps (repeat temp)
-         bindings (interleave
-                   temps (cons nil (map #(list cons % temp) forms))
-                   (repeat s) temps)]
-     `(let [~@bindings]
-        ~temp))))
+    (let [temp (gensym)
+          temps (repeat temp)
+          bindings (interleave
+                     temps (cons nil (map #(list cons % temp) forms))
+                     (repeat s) temps)]
+      `(let [~@bindings]
+         ~temp))))
 
 (defmacro state->
   "'Stateful' Arrow. Takes a state and threads it through a set
-  of symbol s-exp pairs where the expression evaluates to
-  [next-state result]. Result will be bound to symbol, and
-  next-state to state. The return will be a seqable of the
-  final state followed by each result."
+			  of symbol s-exp pairs where the expression evaluates to
+			  [next-state result]. Result will be bound to symbol, and
+			  next-state to state. The return will be a seqable of the
+			  final state followed by each result."
   ([state & forms]
-   (let [pairs (map #(if (even? (count %)) % (cons (gensym) %)) (partition-all 2 forms))
-         results (reverse (map first pairs))
-         bindings (mapcat (fn [[s form]] [[state s] form]) pairs)]
-     `(let [~@bindings]
-        [~state ~@results]))))
+    (let [pairs (map #(if (even? (count %)) % (cons (gensym) %)) (partition-all 2 forms))
+          results (reverse (map first pairs))
+          bindings (mapcat (fn [[s form]] [[state s] form]) pairs)]
+      `(let [~@bindings]
+         [~state ~@results]))))
+
+(defn- -obj-> [f preds args]
+  (fn
+    ([] (apply f args))
+    ([arg] (-obj-> f preds (map #(or %1 %2) (preds arg) args)))))
+
+(defn obj->
+  "Objective arrow. Takes a function and predicates. Returns a function that,
+			when called without a parameter, evalutes the function by passing in
+      the same number of args as there are predicates. If not set each arg
+      is nil. When called with a parameter, evaluates each predicate against
+      that parameter and sets the corresponding arg to the result if not falsey."
+  [f & preds]
+  (-obj-> f (apply juxt preds) (vec (repeat (count preds) nil))))
--- test/seqcess/core_test.clj
+++ test/seqcess/core_test.clj
@@ -7,31 +7,41 @@
   "I'm hoping to come up with my own testing framework... this'll do for now"
 
   (testing "Seeking arrow with"
-    (testing "no binding"
-      (is (nil?              (seq-> _)))
-      (is (= '(1)            (seq-> _ 1)))
-      (is (= '(5 4 3 2 1)    (seq-> _ 1 2 3 4 5)))
-      (is (= '((1 2 3 4 5))  (seq-> _ (range 1 6)))))
-    (testing "simple binding"
-      (is (= '(nil)          (seq-> s s)))
-      (is (= '((nil) nil)    (seq-> s s s)))
-      (is (= '(true 2 (1 2)) (seq-> s (range 1 3) (last (first s)) (< (first (second s)) (first s))))))
-    (testing "positional destructuring"
-      (is (= '(nil)          (seq-> [s] s)))
-      (is (= '(nil nil)      (seq-> [s] s s)))
-      (is (= '(32 16 8 4 2)  (seq-> [s] 2 (* 2 s) (* 2 s) (* 2 s) (* 2 s))))
-      (is (= '(8 5 3 2 1 1)  (seq-> [x y] 1 1 (+ x y) (+ x y) (+ x y) (+ x y)))))
-    (testing "advanced destructing"
-      (is (= (list {:name "Crab", :id 3}
-                   {:name "Bob", :sex :male, :id 2}
-                   {:name "Alice", :sex :female, :id 1})
-             (seq-> [{id :id :or {id 0}}]
-                    {:name "Alice" :sex :female :id (inc id)}
-                    {:name "Bob" :sex :male :id (inc id)}
-                    {:name "Crab" :id (inc id)})))))
+           (testing "no binding"
+                    (is (nil?              (seq-> _)))
+                    (is (= '(1)            (seq-> _ 1)))
+                    (is (= '(5 4 3 2 1)    (seq-> _ 1 2 3 4 5)))
+                    (is (= '((1 2 3 4 5))  (seq-> _ (range 1 6)))))
+           (testing "simple binding"
+                    (is (= '(nil)          (seq-> s s)))
+                    (is (= '((nil) nil)    (seq-> s s s)))
+                    (is (= '(true 2 (1 2)) (seq-> s (range 1 3) (last (first s)) (< (first (second s)) (first s))))))
+           (testing "positional destructuring"
+                    (is (= '(nil)          (seq-> [s] s)))
+                    (is (= '(nil nil)      (seq-> [s] s s)))
+                    (is (= '(32 16 8 4 2)  (seq-> [s] 2 (* 2 s) (* 2 s) (* 2 s) (* 2 s))))
+                    (is (= '(8 5 3 2 1 1)  (seq-> [x y] 1 1 (+ x y) (+ x y) (+ x y) (+ x y)))))
+           (testing "advanced destructing"
+                    (is (= (list {:name "Crab", :id 3}
+                                 {:name "Bob", :sex :male, :id 2}
+                                 {:name "Alice", :sex :female, :id 1})
+                           (seq-> [{id :id :or {id 0}}]
+                                  {:name "Alice" :sex :female :id (inc id)}
+                                  {:name "Bob" :sex :male :id (inc id)}
+                                  {:name "Crab" :id (inc id)})))))
 
   (testing "Stateful arrow"
-    (is (= '(1 false true)
-           (state-> state
-                    small [0 true]
-                    scary [(inc state) (not small)])))))
+           (is (= '(1 false true)
+                  (state-> state
+                           small [0 true]
+                           scary [(inc state) (not small)]))))
+
+  (testing "Objective arrow"
+           (is (= "Hello Alice. You are a 30 year old robot."
+                  (((obj-> (fn [name age gender] (str "Hello " name ". You are a " age " year old " gender "."))
+                          :name :age :gender)
+                     {:gender "robot" :age 30 :name "Alice"}))))
+           (is (= "one2"
+               ((((obj-> #(str %1 %2 %3) :first :second :third) {:second 2}) {:first "one"}))))
+           (is (= "bananabananabanana"
+                  (((obj-> #(str %1 %2 %3) identity identity identity) "banana"))))))
